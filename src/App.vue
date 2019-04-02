a4990b8b9c706555318e37cd86802b4a3b3ddb49
Andrés Becerra Gómez <abecerra@getinsoft.com>
2019-04-02 16:24:15 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <img src="./assets/logo.png">
+      <h1>{{ msg }}</h1>
+      <button class="btn btn-primary">Enviar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg: 'Hello world!!'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  @import './css/main.css';
+</style>
